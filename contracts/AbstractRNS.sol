0282878cc1d185982f31f79ad193831bfce6bc0a
Juraj Piar <juraj.piar@icloud.com>
2021-10-25 18:40:12 +0100

    feat: upgrades sol to v 0.8.4

--- contracts/AbstractRNS.sol
+++ contracts/AbstractRNS.sol
@@ -1,16 +1,30 @@
-pragma solidity ^0.5.0;
+pragma solidity ^0.8.4;
 
-contract AbstractRNS {
-    function owner(bytes32 node) public view returns(address);
-    function resolver(bytes32 node) public view returns(address);
-    function ttl(bytes32 node) public view returns(uint64);
-    function setOwner(bytes32 node, address ownerAddress) public;
-    function setSubnodeOwner(bytes32 node, bytes32 label, address ownerAddress) public;
-    function setResolver(bytes32 node, address resolverAddress) public;
-    function setTTL(bytes32 node, uint64 ttlValue) public;
+abstract contract AbstractRNS {
+    function owner(bytes32 node) public view virtual returns (address);
+
+    function resolver(bytes32 node) public view virtual returns (address);
+
+    function ttl(bytes32 node) public view virtual returns (uint64);
+
+    function setOwner(bytes32 node, address ownerAddress) public virtual;
+
+    function setSubnodeOwner(
+        bytes32 node,
+        bytes32 label,
+        address ownerAddress
+    ) public virtual;
+
+    function setResolver(bytes32 node, address resolverAddress) public virtual;
+
+    function setTTL(bytes32 node, uint64 ttlValue) public virtual;
 
     // Logged when the owner of a node assigns a new owner to a subnode.
-    event NewOwner(bytes32 indexed node, bytes32 indexed label, address ownerAddress);
+    event NewOwner(
+        bytes32 indexed node,
+        bytes32 indexed label,
+        address ownerAddress
+    );
 
     // Logged when the owner of a node transfers ownership to a new account.
     event Transfer(bytes32 indexed node, address ownerAddress);
